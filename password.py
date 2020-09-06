0111dd1fa948deb5051b82ae8066b2a83b2000dd
Eumenes Mau <ppqqla6248@hotmail.com>
2020-09-07 00:37:09 +0800

    refresh n > 0

--- password.py
+++ password.py
@@ -1,17 +1,17 @@
 password = 'a123456'
 n = 3
-while True:
+while n > 0:
+	n = n -1
 	pwd = input('請輸入密碼')
 	if pwd == password:
 		print('登入成功!')
 		break
 	else:
-		n = n - 1
-		print('密碼錯誤，你還剩下', n,'次機會')
-		if n == 0:
-			print('沒機會了，要鎖帳號囉')
-			break
-
+		print('密碼錯誤')
+		if n > 0:
+			print('你還剩下', n,'次機會')
+		else:
+			print('沒機會了!要鎖帳號啦~')
 		
 
 
